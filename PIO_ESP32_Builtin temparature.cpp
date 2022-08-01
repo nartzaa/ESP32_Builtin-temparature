031abb5a13153b0cf798a0cb861c13c80e4267d6
nartzaa <39677740+nartzaa@users.noreply.github.com>
2022-08-01 15:39:58 +0700

    Create PIO_ESP32_Builtin temparature.cpp

---
+++ PIO_ESP32_Builtin temparature.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+  uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t temprature_sens_read();
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  Serial.print("Temperature: ");
+  Serial.print(temprature_sens_read());
+  Serial.print(" F : ");
+  Serial.print((temprature_sens_read() - 32) / 1.8);
+  Serial.println(" C");
+  delay(3000);
+}
